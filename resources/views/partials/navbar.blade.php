789d22f9591c0057b4265c6467724c0018202f29
Wayan Tisna <wayantisna74@gmail.com>
2018-03-28 10:26:35 +0800

    Update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -47,7 +47,7 @@
                         </a>
 
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
+                            {{ csrf_field() }}
                         </form>
                     </div>
                 </li>
