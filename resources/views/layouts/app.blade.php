7a92c906e7ead25c3dc1470b42ebba4c3c1c7eb2
Satya Kresna <satyakresna6295@gmail.com>
2018-04-08 12:36:50 +0000

    Update favicon for apple icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/apple-icon.png') }}">
     <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
