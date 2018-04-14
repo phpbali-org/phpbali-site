d2f3ea84f16efca6377a65bfe6ca2ab77542a873
Dody <prasetyodody17@gmail.com>
2018-04-14 12:30:12 +0800

    Fix minor bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
-    <link rel="icon" type="image/png" href="{{ secure_asset('img/favicon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
 
@@ -199,10 +199,10 @@
 
     <!-- Scripts -->
     <!--   Core JS Files   -->
-    <script src="{{ secure_asset('js/manifest.js') }}" type="text/javascript"></script>
-    <script src="{{ secure_asset('js/vendor.js') }}" type="text/javascript"></script>
-    <script src="{{ secure_asset('js/script.js') }}" type="text/javascript"></script>
-    <script src="{{ secure_asset('js/sweetalert2.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/manifest.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/vendor.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/script.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/sweetalert2.js') }}" type="text/javascript"></script>
     @include('partials.message')
     @if($event)
         <script>
@@ -211,7 +211,7 @@
                 var mapOptions = {
                     zoom: 13,
                     center: myLatlng,
-                    scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+                    scrollwheel: false,
                     styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
                 };
 
@@ -221,9 +221,8 @@
                     position: myLatlng,
                     title:"<?php echo $event->place; ?>"
                 });
-                // To add the marker to the map, call setMap();
                 marker.setMap(map);
-            }
+            };
         </script>
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAuNtNC5mUcvdU0L2RnHlsqaXLe8Ht4Ddw&callback=initMap" async defer></script>
     @endif
