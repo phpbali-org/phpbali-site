251a33aa62e10a226eca3b7087bc96a1c9452d8c
Satya Kresna <satyakresna6295@gmail.com>
2018-10-24 07:42:00 +0800

    Make code clean and tidy in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,17 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'github_id', 'photos', 'website','verified','about','verify_token','slug', 'is_staff'
+        'name',
+        'email',
+        'password',
+        'github_id',
+        'photos',
+        'website',
+        'verified',
+        'about',
+        'verify_token',
+        'slug',
+        'is_staff',
     ];
 
     /**
