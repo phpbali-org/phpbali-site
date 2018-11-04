5089953de4ceb35ee2644f8b360e209bcdee9bb0
Satya Kresna <satyakresna6295@gmail.com>
2018-11-04 09:44:32 +0800

    Add start and end datetime column in event table

---
+++ database/migrations/2018_11_03_150528_add_datetime_to_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDatetimeToEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->dateTime('start_datetime');
+            $table->dateTime('end_datetime');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            //
+        });
+    }
+}
