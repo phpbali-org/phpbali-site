e3f1fe31932fd4f05e6906c3884500baadd08453
Satya Kresna <satyakresna6295@gmail.com>
2019-05-13 17:24:08 +0800

    Update package.json and webpack mix.js setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,28 +10,6 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.options({
-    cleanCss: {
-        level: {
-            1: {
-                specialComments: 'none'
-            }
-        }
-    }
-});
 
-mix.styles([
-	'resources/assets/dashboard-template/css/bootstrap.min.css',
-	'resources/assets/dashboard-template/css/sidebar-nav.min.css',
-	'resources/assets/dashboard-template/css/animate.css',
-	'resources/assets/dashboard-template/css/style.css',
-	'resources/assets/dashboard-template/css/blue-dark.css'
-	], 'public/css/app-dashboard.css');
-mix.scripts([
-	'resources/assets/dashboard-template/js/jquery.js',
-	'resources/assets/dashboard-template/js/bs.js',
-	'resources/assets/dashboard-template/js/jquery.slimscroll.js',
-	'resources/assets/dashboard-template/js/sidebar-nav.min.js',
-	'resources/assets/dashboard-template/js/waves.js',
-	'resources/assets/dashboard-template/js/custom.js'
-	], 'public/js/app-dashboard.js');
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
