cee68855b8cf974229be576f3ca83fec65ae5b8d
Satya Kresna <satyakresna6295@gmail.com>
2019-05-18 16:03:56 +0800

    Make gravatar size better

--- app/helpers.php
+++ app/helpers.php
@@ -4,7 +4,7 @@ function gravatar_url($email) {
     $email = md5($email);
 
     return "https://gravatar.com/avatar/{$email}?" . http_build_query([
-        's' => '60',
+        's' => '240',
         'd' => 'identicon'
     ]);
 }
