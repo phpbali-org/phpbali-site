f1453d6c9a76618ad97a996f20bb8965406d47ec
Satya Kresna <satyakresna6295@gmail.com>
2019-05-19 17:44:13 +0800

    Create meta tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,19 @@
         <meta charset="utf-8">
         <title>{{ config('app.name', 'Laravel') }}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="title" content="PHPBali">
+        <meta name="description" content="Komunitas pemrograman PHP di Bali. Diskusi tentang PHP dan pengembangan web">
+        <meta name="image" content="{{ asset('img/phpbali-logo.png') }}">
+        <link rel="icon" type=image/png href="{{ asset('favicon.png') }}">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="{{ config('app.url') }}">
+        <meta property="og:title" content="PHPBali">
+        <meta property="og:description" content="Komunitas pemrograman PHP di Bali. Diskusi tentang PHP dan pengembangan web">
+        <meta property="og:image" content="{{ asset('img/phpbali-logo.png') }}">
+        <meta property="twitter:url" content="{{ config('app.url') }}">
+        <meta property="twitter:title" content="PHPBali">
+        <meta property="twitter:description" content="Komunitas pemrograman PHP di Bali. Diskusi tentang PHP dan pengembangan web">
+        <meta property="twitter:image" content="{{ asset('img/phpbali-logo.png') }}">
         <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
         @yield('style')
