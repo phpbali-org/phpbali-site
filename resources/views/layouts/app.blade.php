a44313d1fbad69c8a3c40183e35e9a5cbb4df2e3
Satya Kresna <satyakresna6295@gmail.com>
2019-06-18 07:42:47 +0800

    Add google analytics when site is production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,18 @@
         <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
         @yield('style')
+        @if (config('app.env') === 'production')
+            <script src="https://www.google-analytics.com/analytics.js" defer></script>
+            <script defer>
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                ga('create', 'UA-140501276-1', 'auto');
+                ga('send', 'pageview');
+            </script>
+        @endif
     </head>
     <body>
         <div class="flex flex-col min-h-screen">
