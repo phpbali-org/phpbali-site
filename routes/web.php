d471949b171822dad57369d53c54fd7b4827731c
Satya Kresna <satyakresna6295@gmail.com>
2019-06-20 09:58:37 +0800

    Allow guest to see detail of event

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('register/github', 'ReservationController@doReservation');
 
 // Event
 Route::get('events', 'EventController@index');
+Route::get('events/{event}', 'EventController@show');
 
 // About
 Route::get('about', 'AboutController@index');
@@ -20,7 +21,6 @@ Route::post('logout', 'AuthController@logout');
 Route::middleware(['auth'])->group(function () {
     Route::get('events/create', 'EventController@create');
     Route::post('events/store', 'EventController@store');
-    Route::get('events/{event}', 'EventController@show');
     Route::put('events/{event}/publish', 'EventController@publish');
     Route::put('events/{event}/unpublish', 'EventController@unpublish');
     Route::put('events/{event}/register', 'EventController@register');
