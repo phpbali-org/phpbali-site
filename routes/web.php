0e8f75e637d9523cc03da03a0e22d974cc6f81c6
Satya Kresna <satyakresna6295@gmail.com>
2019-06-23 11:58:22 +0800

    Group route based on authentication and roles by middleware.

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,27 @@ Route::get('login/github/callback/{events?}/{event_slug?}', 'AuthController@hand
 Route::post('logout', 'AuthController@logout');
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('events/create', 'EventController@create');
-    Route::post('events/store', 'EventController@store');
-    Route::put('events/{event}/publish', 'EventController@publish');
-    Route::put('events/{event}/unpublish', 'EventController@unpublish');
+
+    // Event and topics
+    Route::middleware(['admin'])->group(function () {
+        Route::get('events/create', 'EventController@create');
+        Route::post('events/store', 'EventController@store');
+        Route::put('events/{event}/publish', 'EventController@publish');
+        Route::put('events/{event}/unpublish', 'EventController@unpublish');
+
+        Route::get('events/{event}/topics/create', 'TopicController@create');
+        Route::post('events/{event}/topics/store', 'TopicController@store');
+
+        Route::get('users', 'UserController@index');
+        Route::get('users/create', 'UserController@create');
+        Route::post('users/store', 'UserController@store');
+    });
+
+    // Register event for authenticated user
     Route::put('events/{event}/register', 'EventController@register');
 
-    Route::get('events/{event}/topics/create', 'TopicController@create');
-    Route::post('events/{event}/topics/store', 'TopicController@store');
-
-    Route::get('events/{event}/attendees/create', 'AttendeeController@create');
-
-    Route::get('users', 'UserController@index');
-    Route::get('users/create', 'UserController@create');
-    Route::post('users/store', 'UserController@store');
+    // Attendee
+    Route::middleware(['admin', 'staff'])->group(function () {
+        Route::get('events/{event}/attendees/create', 'AttendeeController@create');
+    });
 });
