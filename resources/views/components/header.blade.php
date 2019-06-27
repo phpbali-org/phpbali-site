77f75bbde474cf65b77d7463108f5b4f9da2e089
Satya Kresna <satyakresna6295@gmail.com>
2019-06-27 22:47:41 +0800

    Prepare for redesign navbar to responsive

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,22 +1,24 @@
 <header class="p-4">
-    <nav class="flex items-center justify-between">
-        <div class="flex items-center flex-shrink-0 text-white mr-auto">
-            <a href="/">
-                <img src="{{ asset('img/phpbali-logo.png') }}" alt="PHPBali logo" width="50" height="50">
-            </a>
-            <a href="/about" class="block lg:inline-block lg:mt-0 text-black font-bold mx-4 {{ isActive('about') }}">
-                TENTANG
-            </a>
-            <a href="/events" class="block lg:inline-block lg:mt-0 text-black font-bold mx-4 {{ isActive('events') }}">
-                KEGIATAN
-            </a>
-            @if (auth()->check())
-                @if (auth()->user()->isStaff() || auth()->user()->isAdmin())
-                    <a href="/users" class="block lg:inline-block lg:mt-0 text-black font-bold mx-4 {{ isActive('users') }}">PENGGUNA</a>
+    <div class="flex items-center justify-between">
+        <a href="/">
+            <img src="{{ asset('img/phpbali-logo.png') }}" alt="PHPBali logo" width="50" height="50">
+        </a>
+        <nav class="mr-auto">
+            <ul class="flex flex-col md:flex-row items-center flex-shrink-0 text-white mr-auto">
+                <a href="/about" class="block lg:inline-block lg:mt-0 text-black font-bold mx-4 {{ isActive('about') }}">
+                    TENTANG
+                </a>
+                <a href="/events" class="block lg:inline-block lg:mt-0 text-black font-bold mx-4 {{ isActive('events') }}">
+                    KEGIATAN
+                </a>
+                @if (auth()->check())
+                    @if (auth()->user()->isStaff() || auth()->user()->isAdmin())
+                        <a href="/users" class="block lg:inline-block lg:mt-0 text-black font-bold mx-4 {{ isActive('users') }}">PENGGUNA</a>
+                    @endif
                 @endif
-            @endif
-        </div>
-        <div class="flex flex-col items-center">
+            </ul>
+        </nav>
+        <div class="flex items-center">
             @if (auth()->check())
                 <img src="{{ auth()->user()->avatar() }}" alt="User profile" width="50" height="50" class="rounded-full">
                 <form action="/logout" method="post">
@@ -27,5 +29,5 @@
                 <a href="/login/github" class="block lg:inline-block lg:mt-0 text-black font-bold">LOGIN</a>
             @endif
         </div>
-    </nav>
+    </div>
 </header>
