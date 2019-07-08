79428aa5442efce09da5e58c062d474b6d4c3f36
Satya Kresna <satyakresna6295@gmail.com>
2019-07-08 22:05:14 +0800

    Implement filter participant and check if the participant has attended

--- resources/views/components/participant.blade.php
+++ resources/views/components/participant.blade.php
@@ -1,4 +1,4 @@
-<div class="rounded-lg overflow-hidden bg-white shadow p-8 m-4 w-3/4">
+<div class="rounded-lg overflow-hidden bg-white shadow p-8 m-4 w-3/4 participant__identity">
     <form class="flex justify-end md:ml-auto" action="/" method="post">
         @csrf
         <input type="checkbox" name="participant_id" class="w-6 h-6 form-checkbox text-green-500"
@@ -10,7 +10,7 @@
             <img src="{{ gravatar_url($participant->user()->first()->email) }}" alt="Attendee's avatar" class="rounded-full md:w-16 max-w-xs" width="50">
         </div>
         <div class="flex flex-col md:ml-4 mt-4 md:mt-0 items-center md:items-start">
-            <p>
+            <p class="participant__name">
                 {{ $participant->user()->first()->name }}
             </p>
             <p class="text-gray-600 text-sm">
