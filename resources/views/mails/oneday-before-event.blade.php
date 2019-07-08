f7662c963d2fd582f9488c73646d0a2a227f1d83
Satya Kresna <satyakresna6295@gmail.com>
2019-07-08 08:45:00 +0800

    Update mail reminder one day before event

--- resources/views/mails/oneday-before-event.blade.php
+++ resources/views/mails/oneday-before-event.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Pengingat {{ $event->name }}
 
-Halo {{ $user->name }}, ingat tanggal {{ $event->eventDate() }} adalah {{ $event->name }} di [tempat] jam {{ $event->eventTime() }}.
+Halo {{ $user->name }}, ingat tanggal {{ $event->eventDate() }} adalah {{ $event->name }} di {{ $event->place_name }} jam {{ $event->eventTime() }}.
 Pastikan kamu datang ya! Untuk info lebih lanjut, silahkan melalui grup Telegram PHPBali https://t.me/phpbali.
 
 Catatan: kamu menerima email ini karena telah mendaftar kegiatan ini di website https://phpbali.com dengan **login with Github**
