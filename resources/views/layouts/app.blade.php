47f686c1caf31f9ee55ea198e10ddb94d303c909
Satya Kresna <satyakresna6295@gmail.com>
2019-10-23 12:26:02 +0800

    Fix: title tag value

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>{{ config('app.name', 'Laravel') }} @if(isset($title)) - {{ $title }} @endif</title>
+        <title>{{ isset($title) ? $title.' - ' : null }}{{ config('app.name') }}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="title" content="PHPBali">
