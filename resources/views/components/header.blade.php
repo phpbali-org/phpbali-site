c98441a99c57512f50d1e8e65d3d039992f3a508
Satya Kresna <satyakresna6295@gmail.com>
2019-11-15 21:11:07 +0800

    fix: header styling in medium viewport

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,39 +1,41 @@
-<header class="p-4 flex items-center justify-between">
-    <label for="menuToggle" class="sidebarIconToggle" aria-label="Toggle Menu" role="button">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="fill-current h-6 w-6"><path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"/></svg>
-    </label>
-    <input type="checkbox" class="menuToggle" id="menuToggle">
-    <label for="menuToggle" aria-label="Hide Menu" role="presentation" class="menuUnderlay"></label>
-    <a href="/" class="ml-12 md:ml-0">
-        <img src="{{ asset('img/phpbali-logo.png') }}" alt="PHPBali logo" width="50" height="50">
-    </a>
-    <nav id="sidebarMenu" class="md:mr-auto md:flex md:items-center">
-        <label for="menuToggle" class="sidebarIconToggle" aria-label="">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" class="fill-current h-6 w-6"><path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"/></svg>
+<header class="p-4 flex">
+    <div class="flex">
+        <label for="menuToggle" class="sidebarIconToggle" aria-label="Toggle Menu" role="button">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="fill-current h-6 w-6"><path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"/></svg>
         </label>
         <input type="checkbox" class="menuToggle" id="menuToggle">
-        <ul class="md:flex md:items-center">
-            <li class="block lg:inline-block md:text-black font-bold m-0 md:mx-4 {{ isActive('about') }}">
-                <a href="/about">
-                    TENTANG
-                </a>
-            </li>
-            <li class="block lg:inline-block md:text-black font-bold m-0 md:mx-4 {{ isActive('events') }}">
-                <a href="/events">
-                    KEGIATAN
-                </a>
-            </li>
-            @if (auth()->check())
-                @if (auth()->user()->isStaff() || auth()->user()->isAdmin())
-                    <li class="block lg:inline-block md:text-black font-bold m-0 md:mx-4 {{ isActive('users') }}">
-                        <a href="/users">PENGGUNA</a>
-                    </li>
+        <label for="menuToggle" aria-label="Hide Menu" role="presentation" class="menuUnderlay"></label>
+        <a href="/" class="ml-12 md:ml-0">
+            <img src="{{ asset('img/phpbali-logo.png') }}" alt="PHPBali logo" width="50" height="50">
+        </a>
+        <nav id="sidebarMenu" class="md:mr-auto md:flex md:items-center">
+            <label for="menuToggle" class="sidebarIconToggle" aria-label="">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" class="fill-current h-6 w-6"><path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"/></svg>
+            </label>
+            <input type="checkbox" class="menuToggle" id="menuToggle">
+            <ul class="md:flex md:items-center">
+                <li class="block lg:inline-block md:text-black font-bold m-0 md:mx-4 {{ isActive('about') }}">
+                    <a href="/about">
+                        TENTANG
+                    </a>
+                </li>
+                <li class="block lg:inline-block md:text-black font-bold m-0 md:mx-4 {{ isActive('events') }}">
+                    <a href="/events">
+                        KEGIATAN
+                    </a>
+                </li>
+                @if (auth()->check())
+                    @if (auth()->user()->isStaff() || auth()->user()->isAdmin())
+                        <li class="block lg:inline-block md:text-black font-bold m-0 md:mx-4 {{ isActive('users') }}">
+                            <a href="/users">PENGGUNA</a>
+                        </li>
+                    @endif
                 @endif
-            @endif
-        </ul>
-    </nav>
+            </ul>
+        </nav>
+    </div>
     <div class="backdrop"></div>
-    <div class="relative">
+    <div class="relative ml-auto">
         <div class="flex items-center">
             @if (auth()->check())
                 <img src="{{ auth()->user()->avatar() }}" alt="User profile" width="50" height="50" class="rounded-full">
