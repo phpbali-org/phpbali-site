fb4cc4f309988c74fe3ec47a49360d869d15ec91
Satya Kresna <satyakresna6295@gmail.com>
2019-11-16 19:23:11 +0800

    refactor: user component

--- resources/views/pages/user/create.blade.php
+++ resources/views/pages/user/create.blade.php
@@ -1,11 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="w-full max-w-full m-auto">
-      <form class="rounded px-8 pt-6 pb-8 mb-4" method="post" action="/users/store">
-          @include('components.user.form', [
-              'user' => new \App\Models\User
-          ])
-      </form>
-    </div>
+    @include('components.user.form', [
+        'user' => new \App\Models\User,
+        'action' => "/users/store",
+    ])
 @endsection
