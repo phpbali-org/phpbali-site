5eefde22be2e37fe4040f1c258e9c67d5fe3ab5a
Satya Kresna <satyakresna6295@gmail.com>
2019-11-16 11:49:32 +0800

    refactor: split lazy load avatar script in a file

--- resources/views/components/event/page.blade.php
+++ resources/views/components/event/page.blade.php
@@ -324,34 +324,8 @@ document.onreadystatechange = function() {
                 });
             });
         }
-
-        // Avatar lazy load
-        const $avatars = document.querySelectorAll('img[data-src]');
-        const config = {
-            rootMargin: '0px 0px 50px 0px',
-            threshold: 0
-        };
-        let loaded = 0;
-
-        let observer = new IntersectionObserver(function (entries, self) {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    preloadImage(entry.target);
-                    self.unobserve(entry.target);
-                }
-            })
-        }, config);
-
-        $avatars.forEach($avatar => {
-            observer.observe($avatar);
-        });
-
-        function preloadImage(img) {
-            const src = img.getAttribute('data-src');
-            if (!src) { return ; }
-            img.src = src;
-        }
     }
 }
 </script>
+<script src="{{ asset('js/lazy-avatar.js') }}" defer></script>
 @endpush
