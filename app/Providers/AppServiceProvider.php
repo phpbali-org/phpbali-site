a91ccaf49335379dcb24070fe3f15fbf92ae8afc
Kresna <satyakresna6295@gmail.com>
2021-01-17 15:37:13 +0800

    run create dirs in production env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,17 +25,16 @@ class AppServiceProvider extends ServiceProvider
         //     View::share('event', $event);
         // }
 
-        $view = config('view.compiled');
-        $logs = config('vercel.logs_path');
-        // $ssr = config('ssr.node.temp_path');
-        foreach ([$view, $logs] as $path) {
-            if (! is_dir($path)) {
-                mkdir($path, 0755, true);
-            }
-        }
-
         if (!app()->isLocal()) {
             \URL::forceScheme('https');
+
+            $view = config('view.compiled');
+            $logs = config('vercel.logs_path');
+            foreach ([$view, $logs] as $path) {
+                if (! is_dir($path)) {
+                    mkdir($path, 0755, true);
+                }
+            }
         }
     }
 
